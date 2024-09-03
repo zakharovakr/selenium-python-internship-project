0d0090b15ffdb6d448242b48a64d846e2da9af63
Kristina Martins <44987714+zakharovakr@users.noreply.github.com>
2024-09-02 21:50:23 -0400

    Complete the task for AQA-348

--- features/environment.py
+++ features/environment.py
@@ -8,7 +8,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 
 
-def browser_init(context):
+# def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
@@ -24,13 +25,28 @@ def browser_init(context):
     # context.driver = webdriver.Firefox(service=service)
 
     ### HEADLESS MODE ####
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    service = Service(ChromeDriverManager().install())
-    context.driver = webdriver.Chrome(
-        options=options,
-        service=service
-    )
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
+
+    ### BROWSERSTACK ###
+    bs_user = 'kristinamartins_Nmo3ke'
+    bs_key = 'wEgiTkSXUsXptJFeCqDz'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os" : "Windows",
+        "osVersion" : "11",
+        'browserName': 'edge',
+        'sessionName': scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
@@ -41,7 +57,8 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    # browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
